2c99d6abde734aa7a8f47cae1832ede2309b0af9
PotatOceanus <39735195+PotatOceanus@users.noreply.github.com>
2021-07-30 13:15:31 +1000

    Initial

--- src/test/java/com/ATUserManagement/AtUserManagementApplicationTests.java
+++ src/test/java/com/ATUserManagement/AtUserManagementApplicationTests.java
@@ -3,7 +3,6 @@ package com.ATUserManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AtUserManagementApplicationTests {
 
 	@Test
